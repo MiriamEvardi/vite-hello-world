6a161114025716fd179d0c18a31561dcc01c6cea
MiriamEvardi <miriamevardi97@gmail.com>
2024-02-28 18:10:04 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+import MyTitle from './components/MyTitle.vue';
+</script>
+
+<template>
+  <MyTitle></MyTitle>
+</template>
+
+<style></style>
---
+++ src/components/MyTitle.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'MyTitle',
+}
+</script>
+
+<template>
+    <h1></h1>
+</template>
+
+<style>
+h1 {
+    color: blueviolet;
+}
+</style>
